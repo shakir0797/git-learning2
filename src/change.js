6bfedc3d755826a63db675c5e1a76fd2fb893c07
Shakir-Ali <shakirali.vii@gamil.com>
2025-09-15 17:51:32 +0500

    Version-3

--- src/change.js
+++ src/change.js
@@ -1,1 +1,2 @@
 console.log('version-1 change file');console.log('version-2');
+console.log('version-3');
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 console.log('version-1 index file');
 console.log('version-2');
+console.log('version-3')
