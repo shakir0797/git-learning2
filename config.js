3cdbbc09868dc7c25d37a65356c0540942c6e63d
Shakir-Ali <shakirali.vii@gamil.com>
2025-09-15 19:41:49 +0500

    Version-12

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 console.log('version-1');
 console.log('Version-4 after discarding version 2 and version 3');
 console.log('version-5 is added');
+console.log('version-12');
+console.log('ammended in version-12');
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 console.log('version-1 index file');
 console.log('this is the version-5');
+console.log('version-12');
