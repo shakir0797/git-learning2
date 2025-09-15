4af7726b7e352398932f26ebe6fc73931dd7c1cc
Shakir-Ali <shakirali.vii@gamil.com>
2025-09-15 19:34:57 +0500

    Version-5

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 console.log('version-1');
 console.log('Version-4 after discarding version 2 and version 3');
+console.log('version-5 is added');
--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 console.log('version-1 index file');
+console.log('this is the version-5');
